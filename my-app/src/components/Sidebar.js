9ec1bb39535f1e7b75c01953ce60640111f915e8
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-07-18 18:44:38 +0000

    Update Sidebar component and App.js

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => (<div>
+    <a href = "#." target= "_blank">MyPotos</a>
+    <a href = "#." target= "_blank">MyIllustrations</a>
+    <a href = "#." target= "_blank">MyPaintings</a>
+
+</div>
+);
+
+export default Sidebar;
