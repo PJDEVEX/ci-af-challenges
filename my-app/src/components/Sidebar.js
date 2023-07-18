d355673b7954ca785855a9ad5afb972fbbcc21aa
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-07-18 19:02:29 +0000

    Style anch elements

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,11 +1,12 @@
+import css from './css/Sidebar.module.css';
 import React from "react";
 
-const Sidebar = () => (<div>
-    <a href = "#." target= "_blank">MyPotos</a>
-    <a href = "#." target= "_blank">MyIllustrations</a>
-    <a href = "#." target= "_blank">MyPaintings</a>
-
-</div>
+const Sidebar = () => (
+    <div className={css.sidebar}>
+        <a href="#." target="_blank">MyPotos</a>
+        <a href="#." target="_blank">MyIllustrations</a>
+        <a href="#." target="_blank">MyPaintings</a>
+    </div>
 );
 
 export default Sidebar;
