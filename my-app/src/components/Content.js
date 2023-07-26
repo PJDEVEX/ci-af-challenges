337b3a68f52ddcbe9be27ffc92e9efc64e9420c1
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-07-26 22:14:37 +0000

    Component life cycle challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,14 +1,30 @@
 // I. Import React and necessary modules
 import React from 'react';
-import css from "./css/Content.module.css";
+
 import { savedPosts } from '../posts.json';
+import css from "./css/Content.module.css";
+
 import PostItem from './PostItem';
+import Loader from './Loader';
+
 
 // II. Content component class
 export class Content extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        // CMP-LC-challenge: step1 constructor set state
+        this.state = {
+            isLoaded: false
+        }
+    }
+
+    // Life cycle challenge: step 3 set time out
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ isLoaded: true });
+        }, 2000)
+    }
 
     render() {
         return (
@@ -20,18 +36,21 @@ export class Content extends React.Component {
 
                 {/* IV. Search Results */}
                 <div className={css.SearchItem}>
-                    {/* V. Use map to iterate through each individual post */}
-                    {savedPosts.map(post => (
-                        // VI. Render the PostItem component for each post
-                        <PostItem key={post.title} post={post} />
-                    ))}
+                    {/* Lifecycle challenge: step 2 Conditional rendering */}
+                    {this.state.isLoaded ? /* V. Use map to iterate through each individual post */
+                        savedPosts.map(post => (
+                            // VI. Render the PostItem component for each post
+                            <PostItem key={post.title} post={post} />
+                        )) : <Loader />}
+
+
                     {/* Step 7: For each post, create a div with a unique key */}
                     {/* <div key={post.title}>
                         {/* Step 8: Display post details */}
-                        {/* <p>Title: {post.title}</p> */}
-                        {/* <p>Artist: {post.name}</p> */}
-                        {/* <img src={post.image} alt="post image" /> */}
-                        {/* <p>Description: {post.description}</p> */}
+                    {/* <p>Title: {post.title}</p> */}
+                    {/* <p>Artist: {post.name}</p> */}
+                    {/* <img src={post.image} alt="post image" /> */}
+                    {/* <p>Description: {post.description}</p> */}
                     {/* </div> */}
                 </div>
             </div>
